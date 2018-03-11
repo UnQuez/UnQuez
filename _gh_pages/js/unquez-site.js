7b13dbfe983a7ef39b0687549adb18c61bf0c1bb
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2018-03-11 14:18:04 +0700

    Add cool scrolling

--- _gh_pages/js/unquez-site.js
+++ _gh_pages/js/unquez-site.js
@@ -4,3 +4,22 @@
  * @link 
  * @license CC-BY-4
  */
+$(function() {
+  var mainTop = $('#main-content').offset().top;
+  var navbar = $('.navbar');
+
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() > mainTop) {
+      if (!navbar.hasClass('fixed-top')) {
+        navbar.removeClass('navbar-transparent navbar-dark bg-dark')
+              .addClass('fixed-top navbar-light bg-light');
+      }
+    } else {
+      if (navbar.hasClass('fixed-top')) {
+        navbar.removeClass('fixed-top navbar-light bg-light')
+              .addClass('navbar-transparent navbar-dark bg-dark');
+      }
+    }
+  });
+
+});
